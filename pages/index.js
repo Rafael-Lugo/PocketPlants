c675f8291fc1157e30fd569f59685306eb7e35fc
Rafael Lugo <rafaelugo@gmx.de>
2025-12-18 10:00:47 +0100

    fix

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,15 @@
 import PlantList from "@/components/Plantlist/PlantList";
+import useSWR from "swr";
+
+
 
 export default function HomePage() {
+  const { data, isLoading, error } = useSWR("/api/plants");
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Failed to Load</p>;
+
+
   return (
     <div>
       <h1>Plantpal App</h1>

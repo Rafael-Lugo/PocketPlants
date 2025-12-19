2c90b3e5fafb1d2f1ee2d230e42f2c25f54d4339
Rafael Lugo <rafaelugo@gmx.de>
2025-12-19 11:03:15 +0100

    search filter

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,32 @@
 import PlantList from "@/components/Plantlist/PlantList";
 import useSWR from "swr";
+import { useState } from "react";
 
-
-
-export default function HomePage({favoritePlantIds, toggleFavorite}) {
-  const { data: plant, isLoading, error } = useSWR("/api/plants");
+export default function HomePage({ favoritePlantIds, toggleFavorite }) {
+  const { data: plants, isLoading, error } = useSWR("/api/plants");
+  const [search, setSearch] = useState("");
 
   if (isLoading) return <p>Loading...</p>;
   if (error) return <p>Failed to Load</p>;
 
+  const filterPlants = plants?.filter((plant) =>
+    plant.name.toLowerCase().includes(search.toLowerCase())
+  );
 
   return (
     <div>
       <h1>Plantpal App</h1>
-      <PlantList plants={plant} 
-      favoritePlantIds={favoritePlantIds}
-      toggleFavorite={toggleFavorite}/>
+      <input
+        type="search"
+        placeholder="Search plants..."
+        value={search}
+        onChange={(event) => setSearch(event.target.value)}
+      />
+      <PlantList
+        plants={filterPlants}
+        favoritePlantIds={favoritePlantIds}
+        toggleFavorite={toggleFavorite}
+      />
     </div>
   );
 }

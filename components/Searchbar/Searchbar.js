bbdeedd330817a3f670f4050ef07fec5e1a520b8
Rafael Lugo <rafaelugo@gmx.de>
2025-12-19 11:45:43 +0100

    searchbar update

---
+++ components/Searchbar/Searchbar.js
@@ -0,0 +1,19 @@
+export default function SearchBar({ search, setSearch }){
+    function handleSearch(searchString){
+        setSearch(searchString);
+    }
+
+    return(
+        <>
+         <input
+        type="search"
+        placeholder="Search plants..."
+        value={search}
+        onChange={(event) => handleSearch(event.target.value)}
+      />
+      <button type="button" onClick={() => handleSearch("")}>
+        Clear
+      </button>
+        </>
+    )
+}
